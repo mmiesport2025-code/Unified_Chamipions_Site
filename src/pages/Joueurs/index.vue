79f2b8d5a79dd873333d01d0bceada49d8ef60b8
Denki <bardey.estelle1@gmail.com>
2026-01-13 18:22:15 +0100

    redirection pour éviter l'accées à jouurs/index.vue

--- src/pages/Joueurs/index.vue
+++ src/pages/Joueurs/index.vue
@@ -1,14 +1,13 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { AllJoueurs } from '@/backend'
-import { RouterLink } from 'vue-router'
-const listeJoueurs = await AllJoueurs()
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+router.replace('/')
 </script>
 <template>
   <section>
-    <h1 class="uppercase">Liste joueurs</h1>
-    <RouterLink v-for="joueur in listeJoueurs" :key="joueur.id" :to="`/Joueurs/${joueur.id}`">
-      <p>{{ joueur.nom }}</p>
-    </RouterLink>
+    <h1 class="uppercase">Non disponible</h1>
   </section>
 </template>
