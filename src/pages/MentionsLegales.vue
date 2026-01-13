67af6b2e3bbb72a3bcf61fbd2cda8d2e8e144726
Denki <bardey.estelle1@gmail.com>
2026-01-13 20:04:44 +0100

    Ajout de la page mention légales

--- src/pages/MentionsLegales.vue
+++ src/pages/MentionsLegales.vue
@@ -1,4 +1,24 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1 class="uppercase">Mentions légales</h1>
+  <section
+    class="mt-[70px] xl:mt-[120px] mx-auto pt-12 flex flex-col gap-y-5 lg:gap-y-8 mb-12 sm:mb-16 lg:mb-20 max-w-[90vw]"
+  >
+    <h1
+      class="uppercase font-Agrandir text-2xl sm:text-4xl lg:text-5xl text-center text-Violet700 w-[900px] max-w-[80vw] mx-auto"
+    >
+      Mentions légales
+    </h1>
+
+    <ul class="list-disc pl-8 flex flex-col gap-y-2 w-full max-w-[800px] px-5 mx-auto">
+      <li><p>UNIFIED CHAMPIONS CLUB</p></li>
+      <li><p>Siège social : 4 Place Lucien Tharradin, 25200 Montbéliard</p></li>
+      <li><p>À propos : https://www.unifiedchampions.fr/</p></li>
+      <li><p>N° SIREN : 921096616</p></li>
+      <li><p>N°TVA : FR32921096616</p></li>
+      <li><p>N°SIRET : 92109661600013</p></li>
+      <li><p>Directeur de la publication : Pascal Chatonnay</p></li>
+      <li><p>Le site est hébergé chez : Nom dont le siège social est situé à : Localisation</p></li>
+    </ul>
+  </section>
 </template>
