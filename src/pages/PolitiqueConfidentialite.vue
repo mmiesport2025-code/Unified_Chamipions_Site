0422ca7ec9d889802ea3c3610f723c63f1b8f588
Denki <bardey.estelle1@gmail.com>
2026-01-13 19:09:21 +0100

    Ajout politique de confidentialité

--- src/pages/PolitiqueConfidentialite.vue
+++ src/pages/PolitiqueConfidentialite.vue
@@ -1,4 +1,287 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1 class="uppercase">Politique de confidentialité</h1>
+  <section
+    class="mt-[70px] xl:mt-[120px] mx-auto pt-12 flex flex-col gap-y-5 mb-12 sm:mb-16 lg:mb-20 max-w-[90vw]"
+  >
+    <h1
+      class="uppercase font-Agrandir text-2xl sm:text-4xl lg:text-5xl text-center text-Violet700 w-[900px] max-w-[80vw] mx-auto"
+    >
+      Politique de confidentialité
+    </h1>
+
+    <p>
+      La présente politique de confidentialité fixe les conditions dans lesquelles toute information
+      (y compris toute donnée à caractère personnel, selon la définition qui en est donnée par le
+      droit applicable à la protection des données) qui est collectée auprès de vous, ou que vous
+      fournissez, par le biais du site Web www.unifiedchampions.fr (ci-après, le « Site ») sera
+      traitée par nos soins. Veuillez lire attentivement ce qui suit pour comprendre nos pratiques
+      s’agissant des données à caractère personnel vous concernant et de la façon dont nous allons
+      les traiter
+    </p>
+    <p>
+      Vos données à caractère personnel sont collectées sur le Site, pour le compte de (ci-après,
+      les « Prestataires »), par UNIFIED CHAMPIONS CLUB, 4 Place Lucien Tharradin, 25200
+      Montbéliard.
+    </p>
+    <p>
+      UNIFIED CHAMPIONS CLUB collecte vos données à caractère personnel (ci-après les « Données »)
+      afin de les utiliser à des fins statistiques et publicitaires dans le respect de la
+      réglementation applicable en matière de protection des données personnelles.
+    </p>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">Contact</h2>
+
+    <p>
+      Les données personnelles collectées sur le site https://www.unifiedchampions.fr/ sont traitées
+      par UNIFIED CHAMPIONS CLUB, 4 Place Lucien Tharradin, 25200 Montbéliard, en sa qualité de
+      responsable du traitement. <br />
+      Ces données peuvent être collectées notamment via Google Analytics, prestataire chargé de la
+      mesure d’audience et de l’analyse statistique de la fréquentation du site.
+    </p>
+
+    <p>
+      Pour toute question, commentaire ou demande concernant la présente politique de
+      confidentialité, vous pouvez nous contacter à l’adresse suivante :
+      unifiedchampionsclub@gmail.com.
+    </p>
+
+    <p>
+      Conformément au Règlement Général sur la Protection des Données (RGPD), vous disposez d’un
+      droit d’accès, de rectification, d’effacement, de limitation, d’opposition et de portabilité
+      de vos données personnelles. Vous pouvez exercer ces droits en nous écrivant à l’adresse
+      ci-dessus.<br />
+      Vous avez également la possibilité d’introduire une réclamation auprès de la CNIL
+      (www.cnil.fr) si vous estimez que vos droits ne sont pas respectés.
+    </p>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">
+      Données collectées auprès de vous
+    </h2>
+
+    <p>
+      Lorsque vous utilisez le Site ou que vous nous contactez par e-mail, par le biais des réseaux
+      sociaux, par SMS, par téléphone ou par toute autre méthode, nous sommes susceptibles de
+      collecter et de traiter les données suivantes vous concernant :
+    </p>
+
+    <ul class="list-disc pl-8 flex flex-col gap-y-2">
+      <li><p>Les Données que vous nous communiquez, notamment :</p></li>
+      <li>
+        <p>
+          Les Données que nous collectons à votre sujet, chaque fois que vous vous rendez sur le
+          Site, notamment :
+        </p>
+      </li>
+      <li>
+        <p>
+          des informations techniques, y compris l’adresse IP, les informations de connexion, le
+          type de navigateur et sa version, le réglage du fuseau horaire, les types et versions des
+          plug-ins de navigateur, le système d’exploitation et la plateforme ;
+        </p>
+      </li>
+      <li>
+        <p>
+          des informations relatives à votre visite, y compris, le parcours de visite complet de
+          l’URL vers, via et depuis le Site (y compris la date et l’heure)
+        </p>
+      </li>
+      <li>
+        <p>
+          les pages que vous avez consultées ou recherchées, les temps de réponse des pages, les
+          erreurs de téléchargement, la durée des visites sur certaines pages, les informations sur
+          l’interaction avec les pages (notamment, le défilement, les clics et les passages de
+          souris) et les méthodes utilisées pour quitter la page. Nous sommes également susceptibles
+          de collecter des Données sur les appareils (y compris les téléphones, les ordinateurs ou
+          autres) depuis lesquels vous vous rendez sur nos Sites et les visitez. Lesdites Données
+          peuvent inclure les identifiants uniques de l’appareil et les informations liées au réseau
+          mobile (y compris le numéro de téléphone), l’adresse IP, la langue et le fuseau horaire,
+          le système d’exploitation, la version matérielle, la localisation des appareils (y compris
+          des localisations géographiques spécifiques, notamment par le biais des signaux GPS,
+          Bluetooth ou WiFi). Les Prestataires, responsables de traitement, sont amenés, sur la base
+          de votre consentement, à collecter, utiliser, transférer, conserver vos Données dans le
+          cadre de l’envoi d’informations institutionnelles ainsi que de la réalisation d’études
+          statistiques en lien avec le présent Site. Les Données enregistrées sont réservées à
+          l’usage des Prestataires et peuvent être communiquées à leurs prestataires sous-traitants.
+          Les Données ainsi collectées sont conservées pour une durée de 3 ans à compter du dernier
+          échange. Vos droits et les modalités de leur exercice sont détaillés ci-après dans la
+          rubrique dédiée.
+        </p>
+      </li>
+    </ul>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">
+      Données que nous collectons à partir d’autres sources
+    </h2>
+
+    <p>
+      Dans le cadre de l’usage de ce site, nous sommes amenés à collecter des données par le compte
+      de tiers éventuels comme Facebook, Twitter ou autres modalités d’inscription via des
+      identifiants d’autres entreprises françaises ou internationales.
+    </p>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">
+      Utilisation des données
+    </h2>
+
+    <p>Nous sommes susceptibles d’utiliser les Données pour :</p>
+
+    <ul class="list-disc pl-8 flex flex-col gap-y-2">
+      <li><p>répondre à vos demandes ;</p></li>
+      <li>
+        <p>
+          gérer votre accès aux services ou vous informer de toute modification apportée aux
+          services
+        </p>
+      </li>
+      <li><p>nous conformer aux exigences légales et réglementaires</p></li>
+      <li>
+        <p>
+          administrer le Site et effectuer des opérations internes, y compris, la résolution de
+          problèmes, l’analyse de données, les tests, la recherche, les statistiques et les enquêtes
+          ;
+        </p>
+      </li>
+      <li>
+        <p>
+          améliorer le Site afin de veiller à ce que le contenu soit présenté de la manière la plus
+          efficace pour vous
+        </p>
+      </li>
+      <li><p>et garantir la sécurité du Site.</p></li>
+    </ul>
+
+    <p>
+      Destinataires des Données<br />
+      Les destinataires des Données sont les Prestataires.
+    </p>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">
+      Divulgation de données vous concernant
+    </h2>
+
+    <p>Nous sommes susceptibles de partager vos Données avec des sociétés tierces, y compris :</p>
+
+    <p>
+      à des fournisseurs de moteurs de recherche et de services d’analytique qui nous aident à
+      améliorer et optimiser le Site.
+    </p>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">
+      Conservation et transfert de données
+    </h2>
+
+    <p>
+      Toutes les Données que vous fournissez sont susceptibles d’être traitées et conservées,
+      transférées ou accessibles par des entités du monde entier, tel que cela est précisé dans la
+      présente politique. Nous prendrons toutes mesures utiles pour veiller à ce que vos Données
+      soient traitées de manière sécurisée et conformément à la présente Politique de
+      confidentialité.
+    </p>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">Cookies</h2>
+
+    <p>
+      Le Site utilise des cookies et autres technologies de suivi afin de vous distinguer des autres
+      utilisateurs du Site. Ces cookies et technologies nous permettent de vous proposer une
+      meilleure expérience lorsque vous naviguez sur le Site, mais également d’améliorer le Site. Si
+      vous souhaitez des informations détaillées sur les cookies et autres technologies que nous
+      utilisons et les finalités pour lesquelles nous les utilisons, veuillez consulter notre
+      Politique relative aux cookies ci-dessous.
+    </p>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">
+      Droits des utilisateurs
+    </h2>
+
+    <p>
+      Vous êtes autorisé, conformément à la législation en vigueur, à vous opposer au traitement ou
+      à demander la restriction du traitement de vos Données, et à en demander l’accès, la
+      rectification, la suppression et la portabilité en nous contactant selon la procédure décrite
+      dans la section « Contact » ci-dessus. Nous sommes, dans ce cadre, susceptibles de vous
+      demander de prouver votre identité.<br />
+      Vous êtes autorisé, conformément à la législation en vigueur, à déposer une plainte auprès
+      d’une autorité de contrôle si vous estimez que notre façon de traiter vos Données enfreint la
+      législation en vigueur. Vous pouvez également, conformément à la législation en vigueur,
+      définir des directives post mortem.<br />
+      Il vous incombe de tenir vos Données exactes et à jour. Vous pouvez mettre vos Données à jour
+      en nous contactant grâce à la procédure décrite dans la section « Contact » ci-dessus.<br />
+      À tout moment, vous êtes libres de retirer votre consentement au traitement de vos Données,
+      sans pour autant porter atteinte à la licéité du traitement fondé sur le consentement effectué
+      avant le retrait de celui-ci.
+    </p>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">
+      Liens vers des sites tiers
+    </h2>
+
+    <p>
+      Le Site et autres communications sont susceptibles de contenir, de temps à autre, des liens en
+      direction et en provenance des sites Web de nos réseaux de partenaires, publicitaires et
+      sociétés affiliées. Si vous cliquez sur un lien vers l’un quelconque de ces sites Web,
+      veuillez noter que lesdits sites Web disposent de leur propre politique de confidentialité et
+      que nous déclinons toute responsabilité quant auxdites politiques.
+    </p>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">
+      Modifications de notre politique de confidentialité
+    </h2>
+
+    <p>
+      Toute modification à notre politique de confidentialité sera publiée sur la présente page
+      avant l’entrée en vigueur de ladite modification et/ou nous vous en informerons par le biais
+      du Site ou, le cas échéant, par e-mail.
+    </p>
+
+    <h2 class="uppercase font-Agrandir text-xl sm:text-3xl lg:text-3xl w-full mt-10">
+      Utilisateur basés au sein de l’union européene
+    </h2>
+
+    <p>
+      Cette section de la présente Politique de confidentialité s’applique uniquement si vous
+      utilisez le Site depuis un État membre de l’Union européenne ; elle complète les informations
+      contenues dans la présente Politique de confidentialité.
+    </p>
+
+    <p>
+      Bases juridiques du traitement des Données : Nous traitons des Données pour les finalités
+      décrites dans la présente Politique de confidentialité et figurant ci-dessus. Les bases
+      juridiques sur lesquelles nous nous appuyons pour traiter les Données incluent le traitement
+      qui :
+    </p>
+
+    <ul class="list-disc pl-8 flex flex-col gap-y-2">
+      <li>
+        <p>
+          est nécessaire pour l’exécution du contrat qui nous lie tel que pour vous fournir les
+          services que vous demandez.
+        </p>
+      </li>
+      <li><p>est nécessaire pour se conformer aux exigences légales;</p></li>
+      <li>
+        <p>est nécessaire pour gérer notre relation avec vous et pour améliorer le Site ; et</p>
+      </li>
+      <li>
+        <p>
+          est fondé sur le consentement de nos Clients tel que pour communiquer avec vous au sujet
+          de nos services. Votre consentement peut ensuite être retiré à tout moment (à l’aide des
+          paramètres de préférence figurant dans les e-mails ou sur nos Sites, ou à l’aide de ce
+          formulaire, ou en nous envoyant un e-mail grâce à la procédure décrite à la section «
+          Contact ») sans que cela n’affecte le caractère légal du traitement fondé sur le
+          consentement obtenu avant son retrait.  Dans certains cas, il pourra vous être demandé de
+          nous communiquer des Données à des fins de traitement, tel que décrites ci-dessus, afin
+          que nous puissions vous permettre d’utiliser toutes les fonctionnalités du Site.
+        </p>
+      </li>
+    </ul>
+
+    <p>
+      Transferts internationaux<br />
+      Dans le cadre du partage de vos Données conformément à la présente Politique de
+      confidentialité, nous sommes susceptibles de transférer vos Données en dehors de l’Espace
+      économique européen (EEE), aux Etats-Unis, au Canada et en Australie. Ces transferts sont
+      encadrés par des clauses contractuelles types.<br />
+      Lorsque nous transférons vos Données en dehors de l’EEE, nous veillons à ce qu’un niveau de
+      protection similaire soit appliqué à ces Données.
+    </p>
+  </section>
 </template>
