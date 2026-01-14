ac2e0133edddeba50843b4c984a3522d6f712e89
Denki <bardey.estelle1@gmail.com>
2026-01-14 08:30:34 +0100

    suppression de l'ancien pocketbase

--- src/pages/Apropos.vue
+++ src/pages/Apropos.vue
@@ -1,4 +1,73 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1 class="uppercase">à propos de nous</h1>
+  <header>
+    <h1 class="uppercase">à propos de nous</h1>
+    <p>Bienvenue chez Unified Champions Club (UC)</p>
+    <p>
+      La communauté e-sport officielle de ton université. Né de la passion d’étudiants et
+      d’enseignants, UC rassemble des joueuses et joueurs, coachs, créatifs et bénévoles autour
+      d’une même vision : faire du e-sport un véritable terrain d’apprentissage, de partage et de
+      performance.
+    </p>
+    <p>
+      Ici, on joue pour gagner, mais surtout pour progresser ensemble. Entre compétitions, projets,
+      et moments de cohésion, UC, c’est bien plus qu’un club : c’est une aventure humaine portée par
+      l’énergie du campus et la passion du jeu.
+    </p>
+  </header>
+  <section>
+    <div>
+      <h2>L'histoire du club</h2>
+      <p>
+        Tout est parti d’une simple idée : populariser la pratique de l’esport au sein de notre
+        campus universitaire. C’est entre 2021 et 2022, au département des Métiers du Multimédia et
+        de l’Internet (MMI) que Pascal Chatonnay, actuel chef de ce même département et chef de
+        l’association, a eu l’idée de bâtir ce club esport.
+      </p>
+      <p>
+        La particularité de ce club ? Toute personne qui s’inscrit et s’investit avec assiduité pour
+        faire vivre le club se fera accorder jusqu’à 0.25 points bonus dans chaque module de sa
+        moyenne.
+      </p>
+    </div>
+    <div>
+      <img src="" alt="" />
+    </div>
+  </section>
+  <section>
+    <div>
+      <img src="" alt="" />
+    </div>
+    <div>
+      <h2>Qui sommes-nous ?</h2>
+      <p>
+        Nous sommes un collectif d’étudiants qui adore jouer aux jeux vidéos compétitifs, qui se
+        serre les coudes dans les bons comme dans les mauvais moments et qui partage ses moments
+        d’extase aussi bien dans le club qu’en dehors.
+      </p>
+      <p>
+        Nous sommes un groupe de passionnés venant pour la plupart de déprtements universitaires
+        autre que MMI afin de se réunir et créer des liens. Nous sommes plus que des amis, nous
+        sommes des mates. Et nous irons jusqu’au bout.
+      </p>
+    </div>
+  </section>
+  <section>
+    <div>
+      <h2>Premières équipes féminines</h2>
+      <p>
+        Cette année, nous avons l’immense joie d’accueillir nos premières équipes féminines parmi
+        nous. Nous en comptons actuellement 2 spécialisées sur Valorant !
+      </p>
+      <p>
+        Cette nouveauté est gage de renouveau et de progrès dans un monde où il est encore compliqué
+        pour la gente féminine de s’affirmer en tant que joueuse esport. C’est pour cela que nous
+        sommes prêt à tendre la main à celles qui seraient partentent à nous rejoindre pour vivre
+        cette formidable aventure !
+      </p>
+    </div>
+    <div>
+      <img src="" alt="" />
+    </div>
+  </section>
 </template>
